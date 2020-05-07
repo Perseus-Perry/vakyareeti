e4c5fd6d4a77e99e50bfe90130e2407f43ba3ddf
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-07 17:35:01 +0530

    Toggle now works

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,20 @@
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+var mode = 1;
+function toggleMode() {
+  if(mode==1) {
+    document.getElementById('mode').href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css";
+    document.getElementById('mode').integrity="sha384-rCA2D+D9QXuP2TomtQwd+uP50EHjpafN+wruul0sXZzX/Da7Txn4tB9aLMZV4DZm";
+    mode = 0;
+  }
+  else {
+    document.getElementById('mode').href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/flatly/bootstrap.min.css";
+    document.getElementById('mode').integrity="sha384-yrfSO0DBjS56u5M+SjWTyAHujrkiYVtRYh2dtB3yLQtUz3bodOeialO59u5lUCFF";
+    mode = 1;
+  }
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
 }
 
-
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-toggleSwitch.addEventListener('change', function(event) {
-  if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark'); //add this
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light', false); //add this
-  }
+$("a.sic").click(function () {
+    $(".search-toggle").animate({
+        width: 'toggle'
+    });
 });
