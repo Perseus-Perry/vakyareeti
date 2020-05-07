b464ca27cffb124bead182e9a5b9daf65dd3b039
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-07 16:32:23 +0530

    Voting system front works

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,12 +1,22 @@
   var socket = io();
+  var init = Number(document.querySelector(".votes").textContent);
 
   function upvote(id) {
+    document.getElementById('upvote').disabled=true;
+    if(document.getElementById('downvote'.disabled==true)){
+      document.getElementById('downvote').disabled=false;
+    }
 
     socket.emit('upvote', id);
-    document.querySelector(".votes").textContent = Number(document.querySelector(".votes").textContent) + 1;
+    document.querySelector(".votes").textContent = var + 1;
   }
 
   function downvote(id) {
+    document.getElementById('downvote').disabled=true;
+    if(document.getElementById('upvote'.disabled==true)){
+      document.getElementById('upvote').disabled=false;
+    }
+
     socket.emit('downvote', id);
-    document.querySelector(".votes").textContent = Number(document.querySelector(".votes").textContent) - 1;
+    document.querySelector(".votes").textContent = var - 1;
   }
