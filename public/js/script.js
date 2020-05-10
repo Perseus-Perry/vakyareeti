5c31f5879b2aa3a6fec0e615d9c88bd362e2648b
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-10 21:11:10 +0530

    Major update

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,19 @@
-var mode = 1;
-
-function toggleMode() {
-  if(mode==1) {
-    document.getElementById('mode').href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css";
-    document.getElementById('mode').integrity="sha384-rCA2D+D9QXuP2TomtQwd+uP50EHjpafN+wruul0sXZzX/Da7Txn4tB9aLMZV4DZm";
-    mode = 0;
-  }
-  else {
-    document.getElementById('mode').href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/flatly/bootstrap.min.css";
-    document.getElementById('mode').integrity="sha384-yrfSO0DBjS56u5M+SjWTyAHujrkiYVtRYh2dtB3yLQtUz3bodOeialO59u5lUCFF";
-    mode = 1;
-  }
-
-}
-
 $("a.sic").click(function () {
     $(".search-toggle").animate({
         width: 'toggle'
     });
 });
+
+function sharePost(id){
+    console.log("ran");
+    const shareData = {
+    title: 'MDN',
+    text: 'Learn web development on MDN!',
+    url: 'https://developer.mozilla.org',
+  };
+  if(navigator.canShare(shareData)){
+    navigator.share(shareData);
+  }else{
+    console.log("unsupported");
+  }
+}
