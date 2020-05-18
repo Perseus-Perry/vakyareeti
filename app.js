e3db146939c50121e90865f02454c9cac5bea912
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-19 02:52:37 +0530

    v0.0.1

--- app.js
+++ app.js
@@ -221,7 +221,8 @@ app.get("/post/:postID", function (req, res) {
                     username: post.username,
                     title: post.title,
                     body: post.body,
-                    votes: post.votes
+                    votes: post.votes,
+                    createdAt : post.createdAt
                 });
               }
             }
--- public/js/vote.js
+++ public/js/vote.js
@@ -4,21 +4,25 @@
   function upvote(id) {
     if(init == document.querySelector(".votes").textContent ){
         document.querySelector(".votes").textContent = init + 1;
+        document.getElementById('votecountbottom').textContent = init + 1;
         socket.emit('upvote', id);
     } else {
         document.querySelector(".votes").textContent = init;
+        document.getElementById('votecountbottom').textContent = init;
         socket.emit('downvote', id);
     }
 
-    
+
   }
 
   function downvote(id) {
     if(init == document.querySelector(".votes").textContent ){
         document.querySelector(".votes").textContent = init - 1;
+        document.getElementById('votecountbottom').textContent = init - 1;
         socket.emit('downvote', id);
     } else {
         document.querySelector(".votes").textContent = init;
+        document.getElementById('votecountbottom').textContent = init;
         socket.emit('upvote', id);
     }
 
