edd26cf644b95693edc25f97fdb7a3db9eb711cb
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-22 14:45:40 +0530

    post page repaired

--- app.js
+++ app.js
@@ -231,7 +231,8 @@ app.get("/post/:postID", function (req, res) {
                     username: post.username,
                     title: post.title,
                     body: post.body,
-                    votes: post.votes
+                    votes: post.votes,
+                    createdAt : post.createdAt
                 });
               }
             }
